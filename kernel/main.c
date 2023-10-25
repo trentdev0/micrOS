2ffb525f391ef9d41f4d3028b8644cafb670f28d
trentdev0 <matth.ewmajf@gmail.com>
2023-10-24 21:24:42 -0400

    Remove physmem examples, check Git history if you want to see them.

--- kernel/main.c
+++ kernel/main.c
@@ -15,25 +15,6 @@ void _start()
 
 	physmem_init();
 
-	for(int i = 0; i < 10; i++)
-	{
-		uint64_t addr = physmem_index_to_address(1, i);
-		uint64_t indx = physmem_address_to_index(1, addr);
-
-		stream_printf(current_stream, "Here's an address: \"0x%lx\"!\r\n", addr);
-		stream_printf(current_stream, "Here's a page index: \"0x%lx\"!\r\n", indx);
-		stream_printf(current_stream, "Here's a free page index: \"0x%lx\"!\r\n", physmem_find_free(1));
-	}
-
-	while(1)
-	{
-		void * my_pointer0 = physmem_alloc();
-		stream_printf(current_stream, "Allocated a page! Here's it's address: 0x%lx!\r\n", (uint64_t)my_pointer0);
-		int my_ret0 = physmem_free(my_pointer0);
-		stream_printf(current_stream, "Freed the page! Got return code: %d.\r\n", my_ret0);
-	}
-	
-
 	interrupt_wipe();
 	interrupt_flush();
 	interrupt_register(0, &interrupt0, 0x8E);
