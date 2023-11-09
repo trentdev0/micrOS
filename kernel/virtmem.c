3d2c85bba256a9bb3539bb1a0905184dd6f16332
trentdev0 <ma.t.t.he.w.ma.jf@gmail.com>
2023-11-08 21:58:18 -0500

    Working on setting up virtual memory in amd64, still reading Intel SDM and AMD documentation...

--- kernel/virtmem.c
+++ kernel/virtmem.c
@@ -15,6 +15,16 @@ extern char __data_start[];
 extern char __data_end[];
 extern char __kernel_end[];
 
+/**
+ *	@brief Set values in a page table entry.
+ *	@param qword The page table entry.
+ *	@param present Is the page present?
+ *	@param write Should the page have write permissons?
+ *	@param access Should ring3 be able to interact with the target?
+ *	@param writethrough Should the target have a writethrough caching policy?
+ *	@param notcacheable Is the target not cacheable?
+ */
+
 int virtmem_init()
 {
 	return 0;
--- kernel/virtmem.h
+++ kernel/virtmem.h
@@ -3,4 +3,48 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef struct
+{
+	/* Is the target present? */
+	uint64_t present : 1;
+	/* Should the target have write permissions? */
+	uint64_t write : 1;
+	/* Should ring3 be able to interact with the target? */
+	uint64_t access : 1;
+	/* Should the target have a writethrough caching policy? */
+	uint64_t writethrough : 1;
+	/* Is the target not cacheable? */
+	uint64_t notcacheable : 1;
+	uint64_t accessed : 1;
+	uint64_t unused0 : 1;
+	uint64_t global : 1;
+	uint64_t unused1 : 51;
+	uint64_t memprotkey : 4;
+	uint64_t noexec : 1;
+} pagetable_lowest_t;
+
+typedef struct
+{
+	/* Is the target present? */
+	uint64_t present : 1;
+	/* Should the target have write permissions? */
+	uint64_t write : 1;
+	/* Should ring3 be able to interact with the target? */
+	uint64_t access : 1;
+	/* Should the target have a writethrough caching policy? */
+	uint64_t writethrough : 1;
+	/* Is the target not cacheable? */
+	uint64_t notcacheable : 1;
+	uint64_t unused0 : 1;
+	
+	/*
+	 *	It's actually used, we just set it to zero in order to allow
+	 *	4KiB pages.
+	 */
+	uint64_t unused1 : 1;
+	uint64_t unused1 : 52;
+	uint64_t memprotkey : 4;
+	uint64_t noexec : 1;
+} pagetable_directory_t;
+
 int virtmem_init();
